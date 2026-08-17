7f95457aa67bbf0acf02f5a7de9a00fcb3d1be99
grushaju <138559162+grushaju@users.noreply.github.com>
2026-08-17 17:32:28 +0300

    init

---
+++ server/src/test/java/kit/penny/clientbus/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kit.penny.clientbus.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ui/src/main/java/kit/penny/clientbus/ui/UiApplication.java
@@ -0,0 +1,13 @@
+package kit.penny.clientbus.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiApplication.class, args);
+    }
+
+}
