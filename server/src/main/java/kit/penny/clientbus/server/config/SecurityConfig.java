71af0eb65075a3def5e91d6c63d2f72a6b81b4f8
PavelGrushin <grushaju@gmail.com>
2026-08-20 00:37:06 +0300

    Add Scalar!

--- server/src/main/java/kit/penny/clientbus/server/config/SecurityConfig.java
+++ server/src/main/java/kit/penny/clientbus/server/config/SecurityConfig.java
@@ -27,7 +27,9 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/scalar/**",
+                                "/actuator/**"
                         ).permitAll()
 
                         .requestMatchers("/api/**")
