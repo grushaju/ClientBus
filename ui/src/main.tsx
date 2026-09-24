a6424ee1bec6e981867cc537f1bc205c7055f628
PavelGrushin <grushaju@gmail.com>
2026-09-25 01:42:55 +0300

    Stage 8.8 CSS editing 1

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,3 +1,23 @@
+import './index.css'
+
+import './pages/InboxPage.css'
+import './pages/ChannelsPage.css'
+import './pages/ClientsPage.css'
+import './pages/ClientDetailsPage.css'
+import './pages/EmployeesPage.css'
+import './pages/SettingsPage.css'
+import './pages/WorkspaceDetailsPage.css'
+import './pages/WorkspacesPage.css'
+import './pages/LoginPage.css'
+
+import './components/layout/AppLayout.css'
+import './components/layout/Sidebar.css'
+
+import './components/employees/Employees.css'
+import './components/channels/Channels.css'
+import './components/clients/Clients.css'
+import './components/workspaces/Workspaces.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
@@ -5,9 +25,6 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router'
 import { AuthProvider } from './auth/AuthContext'
 
-import './index.css'
-import './styles/employees.css'
-import './styles/settings.css'
 import {WorkspaceProvider} from "./workspace/WorkspaceContext";
 
 ReactDOM.createRoot(
